ecc554eaec2309b3323041c4a2e8f171eb46a128
praveenIsuruPeiris <ezxhale@gmail.com>
2023-10-06 13:26:47 -0400

    Changes to post
    Fetch

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,9 @@ class Post extends Model
 {
     use HasFactory;
 //    protected $fillable =['title', 'excerpt', 'body'];
-        protected $guarded = ['id'];
+        protected $guarded = [];
+
+        protected $with = ['category', 'author'];
 
 
 
